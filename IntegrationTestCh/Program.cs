465f9777208706c85c6e8466ba83b4d12f72825d
lorenz.goerne <lorenz.goerne@lmtnet.de>
2020-09-16 21:35:19 +0200

    Mit Statistik

---
+++ IntegrationTestCh/Program.cs
@@ -0,0 +1,17 @@
+﻿using ChEngine;
+using System;
+using System.Linq;
+
+namespace IntegrationTestCh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Engine e = new Engine(isWhite: true);
+
+            // let the engine crunch on this
+            e.ReactToMove(Enumerable.Empty<Move>());
+        }
+    }
+}
---
+++ TestCh/PerformanceTest.cs
@@ -0,0 +1,54 @@
+﻿using ChEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TestCh
+{
+    [TestClass()]
+    public class PerformanceTest
+    {
+        [TestMethod()]
+        public void TestStartPositionPerformance()
+        {
+            Engine e = new Engine(isWhite: true);
+
+            // let the engine crunch on this
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            e.ReactToMove(Enumerable.Empty<Move>());
+            stopwatch.Stop();
+
+            // Dump
+            string resultJson = JsonConvert.SerializeObject(new PerformanceTestModel()
+            {
+                Date = DateTime.Now,
+                Depth = e.Statistics.MaxDepth,
+                Nodes = e.Statistics.NodesVisited,
+                Evaluation = e.Statistics.Evaluation,
+                Duration = stopwatch.Elapsed,
+            });
+
+            // copy to file
+            string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+            path = Path.Combine(path, "PerformanceTest.json");
+            File.WriteAllText(path, resultJson);
+        }
+    }
+
+    public class PerformanceTestModel
+    {
+        public DateTime Date { get; set; }
+        public int Depth { get; set; }
+        public int Nodes { get; set; }
+        public double Evaluation { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public int NodesPerSecond => (int)(Nodes / Duration.TotalSeconds);
+
+    }
+}
